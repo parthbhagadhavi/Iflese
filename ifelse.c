0239cedec4b2a0ed255383b244b7cff85cb507e4
parth gadhavi <162983927+parthbhagadhavi@users.noreply.github.com>
2024-04-18 15:12:03 +0530

    C language

---
+++ ifelse.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+int a;
+clrscr();
+printf("enter the value of a /n");
+scanf("%d",&a);
+printf("value of a is %d/n",a);
+
+if(a<0){
+
+printf("a number is negative %d",a);
+}
+else-if(a>0){
+
+printf("a number is positive %d",a);
+}
+else{
+
+printf("a value is  zero");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+getch();
+
+}
