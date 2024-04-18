9b7fb6e35eea7fd741d2fabdcf7c0a31d8b28297
parth gadhavi <162983927+parthbhagadhavi@users.noreply.github.com>
2024-04-18 15:13:17 +0530

    If

---
+++ if.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+int a;
+int b;
+clrscr();
+
+printf("enter the value of a \n");
+scanf("%d",&a);
+printf("value of a is %d\n",a);
+
+printf("enter the value of b \n");
+scanf("%d",&b);
+printf("value of a is %d\n",b);
+
+if(a>b){
+
+printf("b value is minimum %d",b);
+
+}
+else{
+
+printf("a value is minimum %d",a);
+}
+
+
+
+
+
+
+
+getch();
+}
